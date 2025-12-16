3c8f82f291b23d48b9500b2ada187ca47ef7730e
Akobir <akobirabduvahobov@gmail.com>
2025-12-16 17:48:09 +0500

    9dars

---
+++ dars9/dars9/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace dars9;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+
+
+
+    }
+
+    static int GetSumm(List<int> nums)
+    {
+        var res = 0;
+
+        foreach (var num in nums)
+        {
+            res += num;
+        }
+
+        return res;
+    }
+
+    static int GetCount(List<string> words)
+    {
+        int counter = 0;
+        foreach (var word in words)
+        {
+            if (word[0] == '$')
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
