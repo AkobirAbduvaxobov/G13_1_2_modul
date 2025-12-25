73bdfe2f05e1ae6b0b05ca1b1feab198a2c5900f
Akobir <akobirabduvahobov@gmail.com>
2025-12-25 17:44:25 +0500

    dars2.1

---
+++ 2.1dars/2.1dars/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Data;
+
+namespace _2._1dars;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        //DateOnly dateOnly = DateOnly.FromDateTime(DateTime.Now);
+        
+        //DateTime dateTime = DateTime.Now;
+        //DateTime utcDateTime = DateTime.UtcNow;
+        //Console.WriteLine(dateTime);
+        //Console.WriteLine(utcDateTime);
+
+        //Console.WriteLine(dateTime.Minute);
+        //Console.WriteLine(dateTime.Hour);
+        //Console.WriteLine(dateTime.Day);
+        //Console.WriteLine(dateTime.Year);
+
+        //DateTime newDateTime = new DateTime();
+
+        DateTime yy = new DateTime(2025, 12, 31, 23, 59, 59);
+        Console.WriteLine(yy);
+
+
+
+        //    Student student = new Student();
+        //    student.StudentId = Guid.NewGuid();
+        //    student.FirstName = "John";
+        //    student.LastName = "Doe";
+        //    student.Phone = "+998900000007";
+        //    student.Email = "Ali@gamil.com";
+
+        //    string student1 = student.ToString();
+        //    Student newStudent = student;
+
+
+
+        //    Console.WriteLine(newStudent);
+
+        //Console.WriteLine(student);
+
+
+        //student.PrintAllInfo();
+
+        //Student student = new Student();
+        //Console.WriteLine("1 chi");
+        //student.PrintAllInfo();
+        //Console.WriteLine("2 chi");
+        //student.StudentId = Guid.NewGuid();
+        //student.FirstName = "John";
+        //student.LastName = "Doe";
+        //student.Phone = "+998900000007";
+        //student.Email = "akobir@gmail.com";
+        //student.PrintAllInfo();
+
+
+
+        //Student student = new Student();
+        //student.Phone = "+998900000007";
+        //Console.WriteLine(student.Phone);
+        //student.StudentId = Guid.NewGuid();
+        //student.PrintPhone();
+        //student.PrintStudentId();
+
+        //Student student2 = new Student();
+        //student2.PrintPhone();
+
+    }
+}
---
+++ 2.1dars/2.1dars/Student.cs
@@ -0,0 +1,55 @@
+﻿namespace _2._1dars;
+
+public class Student
+{
+    public Guid StudentId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public DateTime BirthDate { get; set; }
+
+
+    // methods
+
+    public int CalculateAge()
+    {
+        DateTime now = DateTime.Now;
+        int age = now.Year - BirthDate.Year;
+        return age;
+    }
+
+    public void HelloG13()
+    {
+        Console.WriteLine("Hello G13 from Student class");
+    }
+
+    public void PrintPhone()
+    {
+        Console.WriteLine($"Student Phone: {Phone}");
+    }
+
+    public void PrintStudentId()
+    {
+        Console.WriteLine($"Student ID: {StudentId}");
+    }
+
+    public void PrintAllInfo()
+    {
+        Console.WriteLine($"ID : {StudentId}");
+        Console.WriteLine($"FN : {FirstName}");
+        Console.WriteLine($"LN : {LastName}");
+        Console.WriteLine($"Email : {Email}");
+        Console.WriteLine($"Phone : {Phone}");
+    }
+
+    public int GetFullNameLength()
+    {
+        return (FirstName + LastName).Length;
+    }
+
+    override public string ToString()
+    {
+        return $"ID : {StudentId}\nFN : {FirstName}\nLN : {LastName}\nEmail : {Email}\nPhone : {Phone}";
+    }
+}
