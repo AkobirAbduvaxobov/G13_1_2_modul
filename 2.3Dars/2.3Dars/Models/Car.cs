4d361ef153d3b4b14f031ffe35fbe4e532d318b9
Akobir <akobirabduvahobov@gmail.com>
2026-01-08 17:00:15 +0500

    2.3 dars

---
+++ 2.3Dars/2.3Dars/Models/Car.cs
@@ -0,0 +1,30 @@
+﻿namespace _2._3Dars.Models;
+
+public class Car
+{
+    public Guid CarId { get; set; }
+    public string Color { get; set; }
+    public string Model { get; set; }
+    public string Brand { get; set; }
+    public decimal Price { get; set; }
+
+    public Car()
+    {
+        Foo();
+    }
+
+    public Car(string color, string model, decimal price, string brand = "GM")
+    {
+        Color = color;
+        Model = model;
+        Brand = brand;
+        Price = price;
+        Foo();
+    }
+
+    public void Foo()
+    {
+        Console.WriteLine("Salom g13");
+    }
+
+}
---
+++ 2.3Dars/2.3Dars/Services/CarService.cs
@@ -0,0 +1,97 @@
+﻿using _2._3Dars.Models;
+
+namespace _2._3Dars.Services;
+
+public class CarService
+{
+    List<Car> Cars;
+    public CarService()
+    {
+        Cars = new List<Car>();
+    }
+
+    public Guid AddCar(Car car)
+    {
+        car.CarId = Guid.NewGuid();
+        Cars.Add(car);
+        return car.CarId;
+    }
+
+    public Car? GetCarById(Guid carId)
+    {
+        foreach(var car in Cars)
+        {
+            if(car.CarId == carId)
+            {
+                return car;
+            }
+        }
+
+        return null;
+    }
+
+    public bool DeleteCar(Guid carId)
+    {
+        foreach(var car in Cars)
+        {
+            if(car.CarId == carId)
+            {
+                Cars.Remove(car);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    //public bool DeleteCar(Guid carId)
+    //{
+    //    var car = GetCarById(carId);
+
+    //    if (car == null)
+    //    {
+    //        return false;
+    //    }
+
+    //    Cars.Remove(car);
+    //    return true;
+    //}
+
+    public List<Car> GetAllCars()
+    {
+        return Cars;
+    }
+
+    public bool UpdateCar(Guid carId, Car newCar)
+    {
+        for (var i = 0; i < Cars.Count; i++)
+        {
+            if (Cars[i].CarId == carId)
+            {
+                Cars[i].Model = newCar.Model;
+                Cars[i].Brand = newCar.Brand;
+                Cars[i].Price = newCar.Price;
+                Cars[i].Color = newCar.Color;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    //public bool UpdateCar(Guid carId, Car newCar)
+    //{
+    //    var car = GetCarById(carId);
+    //    if (car == null)
+    //    {
+    //        return false;
+    //    }
+
+    //    car.Model = newCar.Model;
+    //    car.Brand = newCar.Brand;
+    //    car.Price = newCar.Price;
+    //    car.Color = newCar.Color;
+
+    //    return true;
+    //}
+}
