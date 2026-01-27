71c2ec5abea9a6806a857dc90aa9eb660803d288
Akobir <akobirabduvahobov@gmail.com>
2026-01-27 17:43:32 +0500

    added

--- 2.11 dars (Project)/2.11 dars (Project)/Program.cs
+++ 2.11 dars (Project)/2.11 dars (Project)/Program.cs
@@ -1,9 +1,8 @@
-﻿namespace _2._11_dars__Project_;
-
-internal class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(Convert.ToString($"{Guid.NewGuid}").Length);
+
     }
 }
+
